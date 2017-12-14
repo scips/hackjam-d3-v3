976b896c9afa98e2d74c3125f2ad1489aa18bdb1
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2017-12-14 21:09:37 +0100

    function getD3 provide d3

--- src/01-getting-started.js
+++ src/01-getting-started.js
@@ -1,12 +1,16 @@
 // Import d3 from d3
-
+import d3 from 'd3';
 /**
  * Create a function called getD3 that returns the imported d3
  * Make this function visible outside of the module
  */
 
+export const getD3 = () => {
+  return d3;
+} 
+
 /**
  * Create a function that will receive in parameter a d3 object and return
- * a function taht will return the d3 object
+ * a function that will return the d3 object
  * Function name is : functionThatReturnsFunction
  */
